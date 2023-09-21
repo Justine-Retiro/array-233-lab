7e39d76690df14e3b96954a523fc1e57265a38b6
Justine Retiro <128771486+Justine-Retiro@users.noreply.github.com>
2023-09-21 16:40:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let input = document.querySelector('input');
+let content = document.getElementById('content');
+let shoes = ["Adidas", "Nike", "World balance"];
+
+function pushArr(){
+    let inputValue = input.value;
+    shoes.push(inputValue);
+    content.innerHTML += "<p>After push: " + shoes.toString() + "</p>";
+}
+
+function popArr(){
+    shoes.pop();
+    content.innerHTML += "<p>After pop: " + shoes.toString() + "</p>";
+}
+
+function sliceArr(){
+    let slicedShoes = shoes.slice(5);
+    content.innerHTML += "<p>After slice: " + slicedShoes.toString() + "</p>";
+}
